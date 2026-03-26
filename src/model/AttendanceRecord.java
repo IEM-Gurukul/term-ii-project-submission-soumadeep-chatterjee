5e0a6749bfc1ddb34a971dbc2b6c5e6f9abdf8fd
Soumadeep Chatterjee <soumadeepchatterjee10@gmail.com>
2026-03-26 23:48:04 +0530

    Add AttendanceRecord model for tracking attendance data

--- src/model/AttendanceRecord.java
+++ src/model/AttendanceRecord.java
@@ -1,4 +1,46 @@
 package model;
 
 public class AttendanceRecord {
+    private String studentId;
+    private String date;
+    private String status;
+
+    public AttendanceRecord(String studentId, String date, String status) {
+        this.studentId = studentId;
+        this.date = date;
+        this.status = status;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "AttendanceRecord{" +
+                "studentId='" + studentId + '\'' +
+                ", date='" + date + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
